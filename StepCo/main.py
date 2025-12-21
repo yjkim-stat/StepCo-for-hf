18f61195ef24bceaf1fcfd374ba09ff2244a5b04
yjkim-stat <yjkim.stat@yonsei.ac.kr>
2025-12-21 14:44:36 +0000

    sanitize

--- StepCo/main.py
+++ StepCo/main.py
@@ -18,6 +18,8 @@ from verification import step_verify_score
 
 config = Config()
 
+def sanitize(val):
+    return val.replace('/', '_')
 
 ## loading dataset
 parser = argparse.ArgumentParser(description="index of math word problem datasets")
@@ -33,7 +35,7 @@ gold_answers = data_loader.get_gold_answers()
 # if dataset_name=='MATH':
 #     subject, level = data_loader.get_other_information()
 # data_loader.print_avg_tokens(problems)
-answer_save_path = os.path.join(config.result_save_root_path, f'{dataset_name}-{config.prompt_strategy}-{config.backend_LLM}.txt')
+answer_save_path = os.path.join(config.result_save_root_path, f'{dataset_name}-{config.prompt_strategy}-{sanitize(config.backend_LLM)}.txt')
 os.makedirs(config.result_save_root_path, exist_ok=True)
 print(f'[INFO] Answer save path: {answer_save_path}')
 
